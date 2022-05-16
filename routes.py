792054a55071302b9fcf0bb8faac4bf3778c5992
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2022-05-16 12:41:58 +0700

    fixing feat

--- routes.py
+++ routes.py
@@ -1,8 +1,12 @@
 from app import app
 from app.controller import AdminController
+from app.controller import UploadCSVController
 # from app.model2.controller import TB_Controller
 # from app.model2.controller import KK_Controller
 
+#upload folder
+UPLOAD_FOLDER = 'storage/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
@@ -11,6 +15,36 @@ def index():
 @app.route('/admin', methods=['GET'])
 def admin():
     return AdminController.index()
+
+@app.route('/admin/upload/accrue-pendapatan', methods=['post'])
+def upload_csv():
+    return UploadCSVController.accrue_pendapatan()
+
+@app.route('/admin/upload/piutang-ar-billed', methods=['post'])
+def upload_csv_piutang_ar_billed():
+    return UploadCSVController.piutang_ar_billed()
+
+@app.route('/admin/upload/piutang-ar-outstanding', methods=['post'])
+def upload_csv_piutang_ar_outstanding():
+    return UploadCSVController.piutang_ar_outstanding()
+
+@app.route('/admin/upload/piutang-ar-unbilled', methods=['post'])
+def upload_csv_piutang_ar_unbilled():
+    return UploadCSVController.piutang_ar_unbilled()
+
+@app.route('/admin/upload/utangp3', methods=['post'])
+def upload_csv_utangp3():
+    return UploadCSVController.utangp3()
+
+@app.route('/admin/upload/utangp3lain', methods=['post'])
+def upload_csv_utangp3lain():
+    return UploadCSVController.utangp3lain()
+
+@app.route('/admin/upload/utang-int-ap', methods=['post'])
+def upload_csv_utang_int_ap():
+    return UploadCSVController.utang_int_ap()
+
+
 
 
 
